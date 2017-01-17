0132968900ca32dabd24e0543812b9dba6e0e00c
KevinMarquette <kevmar@gmail.com>
2017-01-16 23:38:47 -0800

    Added base module files

---
+++ build.ps1
@@ -0,0 +1,21 @@
+<#
+.Description
+Installs and loads all the required modules for the build.
+.Author
+Warren F. (RamblingCookieMonster)
+#>
+
+[cmdletbinding()]
+param ($Task = 'Default')
+
+# Grab nuget bits, install modules, set build variables, start build.
+Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
+
+Install-Module Psake, PSDeploy, BuildHelpers -force
+Install-Module Pester -RequiredVersion 3.4.2 -Force
+Import-Module Psake, BuildHelpers
+
+Set-BuildEnvironment
+
+Invoke-psake -buildFile .\psake.ps1 -taskList $Task -nologo
+exit ( [int]( -not $psake.build_success ) )
