4c7ea587165530e705da30f63a1e28d8562c2d85
KevinMarquette <kevmar@gmail.com>
2017-01-20 22:46:16 -0800

    updated layout enging support

--- PSGraphViz/Public/Invoke-GraphViz.ps1
+++ PSGraphViz/Public/Invoke-GraphViz.ps1
@@ -11,26 +11,40 @@ function Invoke-GraphViz
         [string[]]
         $Path,
 
-        [ValidateSet('DOT')]
-        $Engine = 'DOT',
-
         [switch]
         $Version,
+
         [string]
         $GraphName,
+
         [string]
         $NodeName,
+
         [string]
         $EdgeName,
+
+        [ValidateSet('jpg','png','gif','imap','cmapx','jp2','json','pdf','plain')]
         [string]
-        $OutputFormat,
+        $OutputFormat = 'png',
+
         [string]
+        [ValidateSet(
+            'Hierarchical',
+            'SpringModelSmall' ,
+            'SpringModelMedium', 
+            'SpringModelLarge', 
+            'Radial',
+            'Circular'
+        )]
         $LayoutEngine,
+        
         [string]
         $ExternalLibrary,
+
         [string]
         $DestinationPath,
-        [string]
+
+        [switch]
         $AutoName
 
     )
@@ -38,6 +52,7 @@ function Invoke-GraphViz
     begin
     {
         $paramLookup = @{
+            # OutputFormat
             Version = 'V'
             Debug = 'v'
             GraphName = 'Gname={0}'
@@ -48,14 +63,33 @@ function Invoke-GraphViz
             ExternalLibrary = 'l{0}'
             DestinationPath = 'o{0}'
             AutoName = 'O'
+
+            #LayoutEngine
+            Hierarchical = 'dot'
+            SpringModelSmall = 'neato'
+            SpringModelMedium = 'fdp'
+            SpringModelLarge = 'sfdp'
+            Radial = 'twopi'
+            Circular = 'circo'
+
         }
-        $graphViz = Resolve-Path  -path 'c:\program files*\GraphViz*\bin\dot.exe'
+        $graphViz = Resolve-Path -path 'c:\program files*\GraphViz*\bin\dot.exe'
+        if($graphViz -eq $null)
+        {
+            throw "Could not find GraphViz installed on this system. Please run 'Find-Package graphviz | Install-Package -ForceBootstrap' or 'Install-GraphViz' to install the needed binaries and libraries. This module just a wrapper around GraphViz and is looking for it in your program files folder."
+        }
     }
     process
     {
         Write-Verbose "Checking lookup table for options"
         $arguments = @()
         
+        if($PSBoundParameters.ContainsKey('LayoutEngine'))
+        {
+            Write-Verbose 'Looking up and replacing rendering engine'
+            $PSBoundParameters['LayoutEngine'] = $paramLookup[$PSBoundParameters['LayoutEngine']]
+        }
+
         foreach($key in $PSBoundParameters.keys)
         {
             Write-Verbose $key
