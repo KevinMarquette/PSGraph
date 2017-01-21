57c408ad9aa66411fbd9d80107a3ff70619cb5f6
KevinMarquette <kevmar@gmail.com>
2017-01-21 12:22:31 -0800

    Fixed issue with module loader

--- PSGraphViz/PSGraphViz.psm1
+++ PSGraphViz/PSGraphViz.psm1
@@ -3,14 +3,17 @@ Write-Verbose "Importing Functions"
 # Import everything in sub folders folder
 foreach($folder in @('private', 'public', 'classes'))
 {
+    
     $root = Join-Path -Path $PSScriptRoot -ChildPath $folder
     if(Test-Path -Path $root)
     {
-        $files = Get-ChildItem -Path $root -Filter *.ps1 -Exclude *.tests.ps1
+        Write-Verbose "processing folder $root"
+        $files = Get-ChildItem -Path $root -Filter *.ps1
 
         # dot source each file
-        $files | ForEach-Object{Write-Verbose $_.name; . $_.FullName}
+        $files | where-Object{ $_.name -NotLike '*.Tests.ps1'} | 
+            ForEach-Object{Write-Verbose $_.name; . $_.FullName}
     }
 }
 
-Export-Modulemember -function (Get-ChildItem -Path "$PSScriptRoot\public\*.ps1").basename
+Export-ModuleMember -function (Get-ChildItem -Path "$PSScriptRoot\public\*.ps1").basename
--- Tests/Project.Tests.ps1
+++ Tests/Project.Tests.ps1
@@ -1,6 +1,10 @@
-Describe "General project validation" {
+$projectRoot = Resolve-Path "$PSScriptRoot\.."
+$moduleRoot = Split-Path (Resolve-Path "$projectRoot\*\*.psm1")
+$moduleName = Split-Path $moduleRoot -Leaf
 
-    $scripts = Get-ChildItem "$PSScriptRoot\.." -Include *.ps1,*.psm1,*.psd1 -Recurse
+Describe "General project validation: $moduleName" {
+
+    $scripts = Get-ChildItem $projectRoot -Include *.ps1,*.psm1,*.psd1 -Recurse
 
     # TestCases are splatted to the script so we need hashtables
     $testCase = $scripts | Foreach-Object{@{file=$_}}         
@@ -13,5 +17,9 @@ Describe "General project validation" {
         $errors = $null
         $null = [System.Management.Automation.PSParser]::Tokenize($contents, [ref]$errors)
         $errors.Count | Should Be 0
+    }
+
+    It "Module '$moduleName' can import cleanly" {
+        {Import-Module (Join-Path $moduleRoot "$moduleName.psm1") -force } | Should Not Throw
     }
 }
