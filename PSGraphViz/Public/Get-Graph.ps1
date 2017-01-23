e7c1816f8478e924e3cb100c6e79cc7302c36a15
KevinMarquette <kevmar@gmail.com>
2017-01-22 21:24:47 -0800

    Moved Get-Graph out into its own file

---
+++ PSGraphViz/Public/Get-Graph.ps1
@@ -0,0 +1,49 @@
+function Get-Graph  
+{
+    [cmdletbinding()]
+    param(
+        [string]
+        $Name,
+
+        [scriptblock]
+        $ScriptBlock,
+
+        [hashtable]
+        $Attributes,
+
+        [string]
+        $type = 'digraph'
+    )
+    
+    begin
+    {
+        if($type -eq 'digraph')
+        {
+            Write-Host "Reset indent"
+            $script:indent = 0
+        }
+
+        "" # Blank line
+        "{0} {1} {2} {{" -f (Get-Indent), $type, $name
+        $script:indent++
+
+        if($Attributes -ne $null)
+        {
+            ConvertTo-GraphVizAttribute -Attributes $Attributes -UseGraphStyle
+        }
+
+        "" # Blank line      
+    }
+
+    process
+    {
+        & $ScriptBlock
+    }
+
+    end
+    {        
+        $script:indent--
+        "$(Get-Indent)}" # Close braces
+        "" #Blank line
+    }
+}
