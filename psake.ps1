0daeea36ebc63b75a416daf3562ad9bd236e835e
KevinMarquette <kevmar@gmail.com>
2017-01-23 21:19:27 -0800

    Testing node issues

--- psake.ps1
+++ psake.ps1
@@ -28,6 +28,8 @@ Task Init {
     "Build System Details:"
     Get-Item ENV:BH* | Format-List
     "`n"
+    "Check for node conflict"
+    Get-Command node | Select-Object *
 }
 
 Task Test -Depends Init  {
