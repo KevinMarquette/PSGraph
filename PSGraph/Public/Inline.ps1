59e317abc12f26ff7030a2f1d0da3f2e442dfc1c
KevinMarquette <kevmar@gmail.com>
2017-01-28 18:02:55 -0800

    fixed failing pster test

--- PSGraph/Public/Inline.ps1
+++ PSGraph/Public/Inline.ps1
@@ -4,10 +4,18 @@ function inline
         .Description
         Allows you to write native DOT format commands inline with proper indention
 
+        .Example
+        graph g {
+            inline 'node [shape="rect";]' 
+        }
         .Notes
         You can just place a string in the graph, but it will not indent correctly. So all this does is give you correct indents.
     #>
-    param([string[]]$InlineCommand)
+    param(
+        # The text to generate inline with the graph
+        [string[]]
+        $InlineCommand
+    )
     
     foreach($line in $InlineCommand)
     {
