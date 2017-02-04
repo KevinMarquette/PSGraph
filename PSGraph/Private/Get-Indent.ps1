be93dbaae9a4daa4e611fb70145e65888c1ab263
KevinMarquette <kevmar@gmail.com>
2017-02-03 22:33:32 -0800

    Fixed issue #9 with depth values out of range

--- PSGraph/Private/Get-Indent.ps1
+++ PSGraph/Private/Get-Indent.ps1
@@ -4,6 +4,10 @@ function Get-Indent
     param($depth=$script:indent)
     process
     {
+        if($depth -eq $null -or $depth -lt 0)
+        {
+            $depth = 0
+        }
         Write-Debug "Depth $depth"
         (" " * 4 * $depth )
     }
