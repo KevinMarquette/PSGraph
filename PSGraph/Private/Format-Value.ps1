e37a63e6cd5b0439a2c3cf91c07e310e9b9e57f4
KevinMarquette <kevmar@gmail.com>
2017-02-06 19:40:01 -0800

    Adjusted HTML detection to be more flexible #15

--- PSGraph/Private/Format-Value.ps1
+++ PSGraph/Private/Format-Value.ps1
@@ -35,7 +35,7 @@ function Format-Value
             switch -Regex ($value)
             {
                 # HTML label, special designation
-                '^<\s*table.*>.*<\/\s*table\s*>$' {
+                '^<\s*table.*>.*' {
                     "<$value>"
                 }
                 # Normal value, no quotes
