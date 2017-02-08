f9d4e20bfec751fd7fc1da6e541658f56de72297
KevinMarquette <kevmar@gmail.com>
2017-02-07 23:12:23 -0800

    Added psscript analyzer pester test

--- build.ps1
+++ build.ps1
@@ -11,9 +11,9 @@ param ($Task = 'Default')
 # Grab nuget bits, install modules, set build variables, start build.
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
-Install-Module Psake, PSDeploy, BuildHelpers -force
+Install-Module Psake, PSDeploy, BuildHelpers, PSScriptAnalyzer -force
 Install-Module Pester -Force -SkipPublisherCheck 
-Import-Module Psake, BuildHelpers
+Import-Module Psake, BuildHelpers, PSScriptAnalyzer
 
 Set-BuildEnvironment
 
