52df81a67152e1771f11a257020094f9803e906e
KevinMarquette <kevmar@gmail.com>
2017-06-03 18:01:10 -0700

    Project settings, skip ci

--- Tests/Feature.Tests.ps1
+++ Tests/Feature.Tests.ps1
@@ -2,8 +2,6 @@ $projectRoot = Resolve-Path "$PSScriptRoot\.."
 $moduleRoot = Split-Path (Resolve-Path "$projectRoot\*\*.psd1")
 $moduleName = Split-Path $moduleRoot -Leaf
 
-#Import-Module (Join-Path $moduleRoot "$moduleName.psm1") -force
-
 Describe "Basic function feature tests" -Tags Build {
 
     Context "Graph" {
