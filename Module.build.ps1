03f56638949361c01b547596fed25e74fb7e1d5c
Kevin Marquette <KMarquette@loandepot.com>
2018-07-14 11:06:31 -0700

    Adjust code coverage

---
+++ Module.build.ps1
@@ -0,0 +1,12 @@
+$Script:ModuleName = 'LDTestFramework'
+$Script:CodeCoveragePercent = 0.0 # 0 to 1
+. $psscriptroot\BuildTasks\InvokeBuildInit.ps1
+
+task Default Build, Test, UpdateSource
+task Build Copy, BuildModule, BuildManifest, Helpify
+task Helpify GenerateMarkdown, GenerateHelp
+task Test Build, ImportModule, FullTests
+
+Write-Host 'Import common tasks'
+Get-ChildItem -Path $buildroot\BuildTasks\*.Task.ps1 |
+    ForEach-Object {Write-Host $_.FullName;. $_.FullName}
