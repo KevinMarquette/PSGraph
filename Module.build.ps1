fd5d87bf2a97211f108d372b8e0348a6386e427a
Kevin Marquette <KMarquette@loandepot.com>
2018-07-16 02:22:48 -0700

    LDX-822 add compile support

--- Module.build.ps1
+++ Module.build.ps1
@@ -3,7 +3,7 @@ $Script:CodeCoveragePercent = 0.0 # 0 to 1
 . $psscriptroot\BuildTasks\InvokeBuildInit.ps1
 
 task Default Build, Test, UpdateSource
-task Build Copy, BuildModule, BuildManifest, Helpify
+task Build Copy, Compile, BuildModule, BuildManifest, Helpify
 task Helpify GenerateMarkdown, GenerateHelp
 task Test Build, ImportModule, FullTests
 
