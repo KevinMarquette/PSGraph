469e604fb03fc1d0868e12b6d14aafa9ad026419
Kevin Marquette <kmarquette@loandepot.com>
2018-12-16 12:14:33 -0800

    midigate sub graph crashes

---
+++ PSGraph/debug.ps1
@@ -0,0 +1,14 @@
+    $graph | & $graphViz
+    digraph g {
+            graph [bb="0,0,88.392,108",
+                    compound=true
+            ];
+            node [label="\N"];
+            Fran├ºois        [height=0.5,
+                    pos="44.196,90",
+                    width=1.1735];
+            S├╕rensen        [height=0.5,
+                    pos="44.196,18",
+                    width=1.2277];
+            Fran├ºois -> S├╕rensen   [pos="e,44.196,36.104 44.196,71.697 44.196,63.983 44.196,54.712 44.196,46.112"];
+    }
