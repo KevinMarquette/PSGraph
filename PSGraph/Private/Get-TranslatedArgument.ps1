c750d40e9eae00e4cdae0246b5b7eb7e21c30006
James O'Neill <P10506111@capita.co.uk>
2020-09-11 13:35:33 +0100

    Support Export-PSgraph with empty path for export to std-out

--- PSGraph/Private/Get-TranslatedArgument.ps1
+++ PSGraph/Private/Get-TranslatedArgument.ps1
@@ -6,7 +6,9 @@ function Get-TranslatedArgument( $InputObject )
     foreach ( $key in $InputObject.keys )
     {
         Write-Debug $key
-        if ( $null -ne $key -and $paramLookup.ContainsKey( $key ) )
+        #A hashtable key can't be null. Don't pass null or empty string. (But allow zero, false etc.)
+        #This means we can get output to std-out by making the destination an empty string.
+        if ( -not [string]::IsNullOrEmpty($InputObject[$key]) -and $paramLookup.ContainsKey( $key ) )
         {
             $newArgument = $paramLookup[$key]
             if ( $newArgument -like '*{0}*' )
